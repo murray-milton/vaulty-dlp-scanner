2d46a66c14a0c1c93abda1a4c9094fefc2782257
murray-milton <miltonmln357@gmail.com>
2025-11-03 11:58:32 +0100

    added the initial layout and pdf upload function for streamlit

--- src/vaulty/app_streamlit.py
+++ src/vaulty/app_streamlit.py
@@ -1,1 +1,15 @@
 """Vaulty Streamlit App (Locally ran DLP scanner UI)."""
+
+import streamlit as stream
+
+"""This will be the basic page set-up for Vaulty demostration"""
+
+stream.set_page_config(page_title="Vaulty - DLP Scanner", layout="centered")
+stream.title("Vaulty 🔒")
+stream.caption("Scan. Detect. Protect.")
+
+""""""
+doc_uploaded = stream.file_uploader(
+    "Please upload a TXT, CSV, or PDF file (10MB or less).",
+    type=["txt", "csv", "pdf"],
+)
